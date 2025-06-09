23701bd89fbfca24984cd1e908db904423798387
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-06-09 18:51:56 +0700

    Membuat fungsi try catch dan throw

--- manualException.cpp
+++ manualException.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 int main()
 {
-
+    try
+    {
+       cout << "Selamat Belajar di Prodi TI UMY" << endl;
+       throw ("halo");
+       cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch(int a)
+    {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch(...)
+    {
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    
+    return 0;
 }
