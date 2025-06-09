3823488e108a5ebf8b4cf7ff299f97e3fc19434c
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-06-09 17:53:27 +0700

    membuat perulangan if

--- ioFIle.cpp
+++ ioFIle.cpp
@@ -31,6 +31,18 @@ int main ()
     //membuat obyek input file steram dalam mode membaca
     ifstream infile;
 
-
+    infile.open("contohfile.txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else
+        cout << "Unable to open file";
+    return 0;
   
 }
