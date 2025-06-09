eea6a86bc648fdbf630c3a6cf563732ed030cb5d
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-06-09 19:01:52 +0700

    Membuat fungsi try catch

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,5 +5,18 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Awal Program " << endl;
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        cout << "data.at (4)" << endl;
+    }
+    catch(exception &e)
+    {
+        cout << e.what() << endl;
+        /* akan dieksekusi karena array data hanya memiliki 3 emelem*/
+    }
+    cout << "Bari Program yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tnanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
