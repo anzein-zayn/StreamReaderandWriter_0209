d7f8e68c4e8de3b2b59f7dcf42aa6d3df67fb826
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-06-09 18:41:54 +0700

    Perulangan while

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -16,7 +16,33 @@ int main ()
     outfile.open(NamaFile + ".txt");
     cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 
-    outfile.open(NamaFile + ".txt", ios ::out);
-
-    cout << ">= Menulis File, \'q\' untuk keluar " << endl;
+    // unclimited loop untuk menulis
+    while(true)
+    {
+        cout << "-";
+        //mendapatkan setiap karakter dalam satu baris
+        getline (cin, baris);
+        //loop akan berhenti jika anda memasukkan karater 4
+        if (baris == "q")
+        break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+  outfile.close();
+    //membuat obyek input file steram dalam mode membaca
+    ifstream infile;
+    infile.open(NamaFile + ".txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else
+        cout << "Unable to open file";
+    return 0;
+  
 }
